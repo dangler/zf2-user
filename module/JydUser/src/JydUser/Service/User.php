6ca44752e5843f42b155b7fe2368cd6c3e59e100
jdangler <jdangler@gmail.com>
2014-06-19 10:31:51 -0400

    improving service manager

--- module/JydUser/src/JydUser/Service/User.php
+++ module/JydUser/src/JydUser/Service/User.php
@@ -43,4 +43,38 @@ class User implements ServiceLocatorAwareInterface
         $em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
         return $em->getRepository('JydUser\Entity\User')->findBy(array('domain' => $domain));
     }
+
+    /**
+     * @param $id
+     * @return JydUser/Entity/User
+     */
+    public function getUserForId($id)
+    {
+        /** @var EntityManager $em */
+        $em = $this->getServiceLocator()->get('doctrine.entitymanager.orm.default');
+        return $em->getRepository('JydUser\Entity\User')->find($id);
+    }
+
+    /**
+     * @param $domain
+     * @param $firstName
+     * @param $lastName
+     * @param $middleName
+     *
+     * @return JydUser/Entity/User
+     */
+    public function createNewUser($domain, $firstName, $lastName, $middleName)
+    {
+        $user = new \JydUser\Entity\User();
+        $user->setDomain($domain);
+        $user->setFirstName($firstName);
+        $user->setMiddleName($middleName);
+        $user->setLastName($lastName);
+
+        /** @var EntityManager $em */
+        $em = $this->getServiceLocator()->get('doctrine.entitymanager.orm.default');
+        $em->persist($user);
+
+        return $em->getRepository('JydUser\Entity\User')->find($id);
+    }
 }
